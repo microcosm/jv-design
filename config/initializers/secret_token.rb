3c0ca651673d02a913f48c357a2c210af9b0b7b4
Andrew McWilliams <andy@jahya.net>
2013-09-29 14:01:57 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jv::Application.config.secret_token = 'af003ddab70fd2d02b39dd8d019e365aca8462df6e3fc44c2fdc3a087f4018ed86298a8b7cade5971c7970666de4d354eb1203ea16c65cd26e516d98155fad40'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Jv::Application.config.session_store :cookie_store, key: '_jv_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Jv::Application.config.session_store :active_record_store
